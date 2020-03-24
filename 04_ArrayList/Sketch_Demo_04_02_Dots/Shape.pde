72561a2093df6d0c57bca1ce79cd4c792e227db0
Cédric Peeters <cedric.peeters13@gmail.com>
2020-03-24 17:19:45 +0100

    fixed rotation of the shapes. exercise complete.

--- 04_ArrayList/Sketch_Demo_04_02_Dots/Shape.pde
+++ 04_ArrayList/Sketch_Demo_04_02_Dots/Shape.pde
@@ -2,16 +2,24 @@ class Shape{
   private PVector pos;
   private color clr;
   private int size = 20, round = 5;
+  private float angle;
 
   //const
   Shape(PVector p, color c){
     this.pos = p;
     this.clr = c;
+    //calculate a random angle to use
+    angle = random(361);
   }
 
   //display
   void display(){
     fill(clr);
-    rect(pos.x, pos.y, size, size, round);
+    pushMatrix();
+    translate(pos.x, pos.y);
+    rotate(radians(angle));
+    rectMode(CENTER);
+    rect(0, 0, size, size, round);
+    popMatrix();
   }
 }
--- 04_ArrayList/Sketch_Demo_04_02_Dots/out/source/Sketch_Demo_04_02_Dots.java
+++ 04_ArrayList/Sketch_Demo_04_02_Dots/out/source/Sketch_Demo_04_02_Dots.java
@@ -50,17 +50,25 @@ class Shape{
   private PVector pos;
   private int clr;
   private int size = 20, round = 5;
+  private float angle;
 
   //const
   Shape(PVector p, int c){
     this.pos = p;
     this.clr = c;
+    //calculate a random angle to use
+    angle = random(361);
   }
 
   //display
   public void display(){
     fill(clr);
-    rect(pos.x, pos.y, size, size, round);
+    pushMatrix();
+    translate(pos.x, pos.y);
+    rotate(radians(angle));
+    rectMode(CENTER);
+    rect(0, 0, size, size, round);
+    popMatrix();
   }
 }
   public void settings() {  size(500, 500); }
